08fc01f4ae6177c9cc81a9aa67ed104a00cbdd52
Nikoh <nikoh@nikoh.it>
2023-03-09 19:14:17 +0100

    better compatibility for future database integration

--- Authentication-server/db/serverData.gd
+++ Authentication-server/db/serverData.gd
@@ -1,8 +1,17 @@
 extends Node
 
-var GameServerIDs = {
-	"Chimera one": { 
+var gameServerList = {
+	"Chimera_one": { 
 	"url": "chimera.nikoh.it",
 	"pippo": "paperino"
+	},
+	"Chimera_two": { 
+	"url": "chimera.nikoh.it",
+	"topolino": "paperone"
+	}
+}
+var gatewayServerList = {
+	"Chimera_gw_one": { 
+	"url": "chimeragw.nikoh.it"
 	}
 }
