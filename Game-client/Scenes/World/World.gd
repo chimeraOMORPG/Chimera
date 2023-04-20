35cc7de74bd5d0ef14b079eea28249260a7300b3
Nikoh <nikoh@nikoh.it>
2023-04-20 22:22:37 +0200

    bugfix

--- Game-client/Scenes/World/World.gd
+++ Game-client/Scenes/World/World.gd
@@ -4,6 +4,7 @@ extends Node2D
 func addSceneOnClient(Place):
 	var x = load('res://Scenes/World/' + Place + '.tscn').instantiate()
 	self.add_child.call_deferred(x, true)
+	rpc_id(1, 'sceneOnClientAdded')
 	prints(Place, 'scena aggiunta')
 
 func _on_child_entered_tree(node):
@@ -11,3 +12,4 @@ func _on_child_entered_tree(node):
 	
 @rpc("authority")
 func sceneOnClientAdded():
+	pass
